63a799c76d619569d705ab1d4b7160ea7beba38e
habvi <a2231250601h@gmail.com>
2023-05-10 12:24:12 +0200

    a

--- .github/sh/norm.py
+++ .github/sh/norm.py
@@ -32,16 +32,31 @@ run_cmd(cmd)
 cmd = "norminette " + norm_check_dir_path +  " > " + out_filepath
 run_cmd(cmd)
 
-ignore1 = "OK"
-ignore2 = "Error!"
-ignore3 = "Missing or invalid 42 header"
-with open(out_filepath) as f:
-    for line in f:
-        print(line)
-        if ignore1 in line or ignore2 in line or ignore3 in line:
-            continue
-        print_color_str(RED, "[NG] ")
-        exit(1)
+def put_lines(lines):
+    for line in lines:
+        print(line, end="")
+    print("-----------------------")
 
-print_color_str(GREEN, "[OK] ")
-exit(0)
+line_ok = "OK"
+line_err = "Error!"
+line_header = "Missing or invalid 42 header"
+result = True
+with open(out_filepath) as f:
+    lines = []
+    for line in f:
+        if line_err in line:
+            if len(lines) >= 2:
+                put_lines(lines)
+                result = False
+            lines = [line]
+        else:
+            if line_header in line:
+                continue
+            lines.append(line)
+
+if result:
+    print_color_str(GREEN, "[OK] ")
+    exit(0)
+else:
+    print_color_str(RED, "[NG] ")
+    exit(1)
