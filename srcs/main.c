8a7c2510792a1957d6fd5771de9b4b275d4f0408
habvi <93956895+habvi@users.noreply.github.com>
2023-05-11 10:48:42 +0200

    create: simple main

---
+++ srcs/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+static void	minishell(void)
+{
+	while (true)
+	{
+		// readline
+		// tokenize
+		// parse
+		// exec
+	}
+}
+
+int	main(void)
+{
+	minishell();
+	return (EXIT_SUCCESS);
+}
