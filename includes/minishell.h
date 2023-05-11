e2a56f9434cec83956a96125a0b1ab2886100c85
habvi <93956895+habvi@users.noreply.github.com>
2023-05-11 13:21:51 +0200

    fix: space

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,10 +1,10 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <unistd.h>
 
 # define EXECVE_ERROR -1
 
