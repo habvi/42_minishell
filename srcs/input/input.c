79fba308b64cd73a87658f832edada585d5d1d24
takira <ca.fie.0220@gmail.com>
2023-05-13 04:04:32 +0900

    update: change prompt outstream stdout to stderr

--- srcs/input/input.c
+++ srcs/input/input.c
@@ -2,11 +2,13 @@
 #include <readline/history.h>
 #include "minishell.h"
 #include "ft_string.h"
+#include <stdio.h>
 
 char	*input_line(void)
 {
 	char	*line;
 
+	rl_outstream = stderr;
 	line = readline(PROMPT_NAME);
 	if (!line)
 		return (NULL);
