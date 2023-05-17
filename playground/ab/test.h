1b6fc231acb441c1c6bc229292f3063a3022c8e9
habvi <93956895+habvi@users.noreply.github.com>
2023-05-17 16:34:56 +0200

    add(test.h): define DUP2_ERROR

--- playground/ab/test.h
+++ playground/ab/test.h
@@ -3,12 +3,13 @@
 
 # define READ_ERROR		-1
 # define WRITE_ERROR	-1
-# define OPEN_ERROR     -1
+# define OPEN_ERROR		-1
 
-# define CHILD_PID	0
-# define FORK_ERROR	-1
-# define WAIT_ERROR	-1
-# define PIPE_ERROR	-1
-# define EXECVE_ERROR  -1
+# define CHILD_PID		0
+# define FORK_ERROR		-1
+# define WAIT_ERROR		-1
+# define PIPE_ERROR		-1
+# define EXECVE_ERROR	-1
+# define DUP2_ERROR		-1
 
 #endif
