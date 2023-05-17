074e516fb053ab25d462756f3a74297838c6096a
habvi <93956895+habvi@users.noreply.github.com>
2023-05-17 16:53:09 +0200

    update(include): -1 -> (-1)

--- playground/ab/test.h
+++ playground/ab/test.h
@@ -1,15 +1,15 @@
 #ifndef TEST_H
 # define TEST_H
 
-# define READ_ERROR		-1
-# define WRITE_ERROR	-1
-# define OPEN_ERROR		-1
+# define READ_ERROR		(-1)
+# define WRITE_ERROR	(-1)
+# define OPEN_ERROR		(-1)
 
 # define CHILD_PID		0
-# define FORK_ERROR		-1
-# define WAIT_ERROR		-1
-# define PIPE_ERROR		-1
-# define EXECVE_ERROR	-1
-# define DUP2_ERROR		-1
+# define FORK_ERROR		(-1)
+# define WAIT_ERROR		(-1)
+# define PIPE_ERROR		(-1)
+# define EXECVE_ERROR	(-1)
+# define DUP2_ERROR		(-1)
 
 #endif
