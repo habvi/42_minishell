bedcc89ded6c7f0be69dacc35f897bbceb7a7f6b
takira <ca.fie.0220@gmail.com>
2023-05-18 22:30:20 +0900

    update(test.sh)

--- playground/ak/test.sh
+++ playground/ak/test.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-assert() {
-  input="$1"
+test() {
+    input="$1"
 
-  expected=`echo $(($input))`
+    expected=`echo $(($input))`
 #   echo "expected:[$expected]"
-  actual=`./a.out "$input"`
+    actual=`./a.out "$input"`
 
-  if [ "$actual" = "$expected" ]; then
-    echo -e "\033[32m[OK] input : $input \\n => actual   = $actual\033[00m\\n"
-  else
-    echo -e "\033[31m[NG] input : $input \\n => actual   = $actual\\n    expected = $expected\033[00m\\n"
-  fi
+    if [ "$actual" = "$expected" ]; then
+        echo -e "\033[32m[OK] input : $input \\n => actual   = $actual\033[00m\\n"
+    else
+        echo -e "\033[31m[NG] input : $input \\n => actual   = $actual\\n    expected = $expected\033[00m\\n"
+    fi
 }
 
+
+echo COMPILE
 make re
+echo ========================= TEST START =========================
+test 0
+test 42
+test "1 + 2"
+test "1 + 2 - 1"
+test "(1 + 2) * 2"
+test "(42 / 2 - 1) / 2"
 
-assert 0
-assert 42
-assert "1 + 2"
-assert "1 + 2 - 1"
-assert "(1 + 2) * 2"
-assert "(42 / 2 - 1) / 2"
+echo ==============================================================
 
-echo END
