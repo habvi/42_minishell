7b481bcd26746d460a5a8fc31064cfdc8c8927cf
habvi <93956895+habvi@users.noreply.github.com>
2023-05-20 22:16:42 +0200

    update(multi_test.py): able to use stdin

--- playground/ab/multistage_pipe_test.py
+++ playground/ab/multistage_pipe_test.py
@@ -22,28 +22,46 @@ def print_color_str_no_lf(color=WHITE, text=""):
 
 # ----------------------------------------------------------
 # run
-def run_cmd(cmd):
-    return subprocess.run(cmd, capture_output=True, text=True, shell=True)
+def run_cmd(stdin=None, cmd=None):
+    return subprocess.run(cmd, input=stdin, capture_output=True, text=True, shell=True)
 
-def run_minishell(cmd):
-    res_minishell = run_cmd(cmd)
+def run_cmd_shell_false(stdin=None, cmd=None):
+    return subprocess.run(cmd, input=stdin, capture_output=True, text=True, shell=False)
+
+def run_minishell(stdin, cmd):
+    res_minishell = run_cmd(stdin, cmd)
     print("=== minishell ===")
     print(cmd)
     print(f'[{res_minishell.stdout}]')
     # print(res_minishell.stderr)
     return res_minishell.stdout
 
-def run_bash(cmd):
-    res_bash = run_cmd(cmd)
+def run_bash(stdin, cmd):
+    res_bash = run_cmd(stdin, cmd)
     print("===== bash =====")
     print(cmd)
     print(f'[{res_bash.stdout}]')
     return res_bash.stdout
 
+# def run_both(stdin, cmd):
+#     m_cmd = [PATH] + cmd.split()
+#     res_minishell = run_cmd_shell_false(stdin, m_cmd)
+#     print("=== minishell ===")
+#     print(m_cmd, len(res_minishell.stdout))
+#     print(f'[{res_minishell.stdout}]')
+
+#     b_cmd = cmd
+#     res_bash = run_cmd(stdin, b_cmd)
+#     print("===== bash =====")
+#     print(b_cmd, len(res_bash.stdout))
+#     print(f'[{res_bash.stdout}]')
+#     print(res_minishell == res_bash)
+#     return res_minishell, res_bash
+
 # ----------------------------------------------------------
 # put
-def put_result(result, ok, ko):
-    if result:
+def put_result(m_res, b_res, ok, ko):
+    if m_res == b_res:
         print_color_str(GREEN, "[OK]")
         ok += 1
     else:
@@ -53,22 +71,39 @@ def put_result(result, ok, ko):
 
 def put_total_result(ok, ko):
     print()
-    print(ok + ko, "test case / ", end="")
     print_color_str_no_lf(GREEN, "OK ")
-    print(ok, ", ", end="")
+    print(f'{ok}, ', end="")
     print_color_str_no_lf(RED, "KO ")
-    print(ko)
+    print(ko, end="")
+    print(f' (test case: {ok + ko})')
 
 # ----------------------------------------------------------
 def main():
     cmd = "make multi"
-    run_cmd(cmd)
+    run_cmd(None, cmd)
 
     ok = 0
     ko = 0
-    m_res = run_minishell(f'{PATH} "echo" "-e" "aaa\\naacc\\nbbb\\nbbcc\\nccc\\naabb\\nabc" "|" "grep" "a" "|" "grep" "c"')
-    b_res = run_bash('echo -e "aaa\\naacc\\nbbb\\nbbcc\\nccc\\naabb\\nabc" | grep a | grep c')
-    ok, ko = put_result(m_res == b_res, ok, ko)
+
+    # m_res, b_res = run_both(None, "echo a")
+    # ok, ko = put_result(m_res, b_res, ok, ko)
+
+    m_res = run_minishell(None, f'{PATH} "echo" "-e" "aaa\\naacc\\nbbb\\nbbcc\\nccc\\naabb\\nabc" "|" "grep" "a" "|" "grep" "c"')
+    b_res = run_bash(None, 'echo -e "aaa\\naacc\\nbbb\\nbbcc\\nccc\\naabb\\nabc" | grep a | grep c')
+    ok, ko = put_result(m_res, b_res, ok, ko)
+
+    m_res = run_minishell(None, f'{PATH} "echo" "a"')
+    b_res = run_bash(None, "echo a")
+    ok, ko = put_result(m_res, b_res, ok, ko)
+
+    stdin = "abcde"
+    m_res = run_minishell(stdin, f'{PATH} "cat" "|" "echo" "-e" "aaa\\naacc\\nbbb\\nbbcc\\nccc\\naabb\\nabc" "|" "grep" "a" "|" "grep" "c"')
+    b_res = run_bash(stdin, 'cat | echo -e "aaa\\naacc\\nbbb\\nbbcc\\nccc\\naabb\\nabc" | grep a | grep c')
+    ok, ko = put_result(m_res, b_res, ok, ko)
+
+    m_res = run_minishell(None, f'{PATH} "echo" "a" "|" "echo" "bb" "|" "echo" "ccc"')
+    b_res = run_bash(None, "echo a | echo bb | echo ccc")
+    ok, ko = put_result(m_res, b_res, ok, ko)
 
     put_total_result(ok, ko)
 
