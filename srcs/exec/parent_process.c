fd5491cbd37610f05b2d7098d1dd1f2d119a54ac
habvi <93956895+habvi@users.noreply.github.com>
2023-05-23 12:56:00 +0200

    fmt(parent_proc): separate func

--- srcs/exec/parent_process.c
+++ srcs/exec/parent_process.c
@@ -1,50 +1,80 @@
 #include <sys/wait.h> // wait
-#include <errno.h>
+#include <errno.h> // ECHILD
 #include "minishell.h"
 #include "ft_dprintf.h"
+#include "libft.h"
 
-int	parent_process(t_command *cmd, t_fd *fd, pid_t pid, int *last_exit_status)
+static int	handle_parent_pipes_except_first(t_fd *fd)
 {
-	int		status;
-	pid_t	wait_pid;
+	if (x_close(fd->prev_fd) == PROCESS_ERROR)
+		return (PROCESS_ERROR);
+	return (EXIT_SUCCESS);
+}
 
-	(void)cmd;
+static int	handle_parent_pipes_except_last(t_fd *fd)
+{
+	fd->prev_fd = fd->pipefd[READ];
+	if (x_close(fd->pipefd[WRITE]) == PROCESS_ERROR)
+		return (PROCESS_ERROR);
+	return (EXIT_SUCCESS);
+}
+
+static int	handle_parent_pipes(t_command *cmd, t_fd *fd)
+{
 	if (!is_first_command(fd->prev_fd))
 	{
-		// SYS_ERROR
-		close(fd->prev_fd);
+		if (handle_parent_pipes_except_first(fd) == PROCESS_ERROR)
+			return (PROCESS_ERROR);
 	}
 	if (!is_last_command(*cmd->next_command))
 	{
-		fd->prev_fd = fd->pipefd[READ];
-		// SYS_ERROR
-		close(fd->pipefd[WRITE]);
-		return (EXIT_SUCCESS);
+		if (handle_parent_pipes_except_last(fd) == PROCESS_ERROR)
+			return (PROCESS_ERROR);
 	}
-	wait_pid = waitpid(pid, &status, 0);
-	if (WIFEXITED(status))
-		*last_exit_status = WEXITSTATUS(status);
+	return (EXIT_SUCCESS);
+}
+
+static int	get_last_command_status(pid_t pid, int *status, int *last_exit_status)
+{
+	pid_t	wait_pid;
+
+	wait_pid = x_waitpid(pid, status, 0);
+	if (WIFEXITED(*status))
+		*last_exit_status = WEXITSTATUS(*status);
 	else
-	{
-		perror("waitpid");
-		return (PROCESS_ERROR);
-	}
+		return (WAIT_ERROR);
+	return (EXIT_SUCCESS);
+}
+
+static int	wait_all_child_process(int status)
+{
 	while (true)
 	{
-		wait_pid = wait(NULL);
-		if (wait_pid == WAIT_ERROR)
+		if (wait(NULL) != WAIT_ERROR)
+			continue ;
+		if (errno == ECHILD && WIFEXITED(status))
+			return (EXIT_SUCCESS);
+		else
 		{
-			if (errno == ECHILD && WIFEXITED(status))
-			{
-				ft_dprintf(STDERR_FILENO, "\nchild exit success! status: %d, last cmd exit status: %d\n", WIFEXITED(status), *last_exit_status);
-				return (EXIT_SUCCESS);
-			}
-			else
-			{
-				perror("wait");
-				return (PROCESS_ERROR);
-			}
+			perror("wait");
+			return (PROCESS_ERROR);
 		}
 	}
+	return (EXIT_SUCCESS);
+}
+
+int	parent_process(t_command *cmd, t_fd *fd, pid_t pid, int *last_exit_status)
+{
+	int	status;
+
+	if (handle_parent_pipes(cmd, fd) == PROCESS_ERROR)
+		exit(EXIT_FAILURE);
+	if (get_last_command_status(pid, &status, last_exit_status) == PROCESS_ERROR)
+		return (PROCESS_ERROR);
+	if (wait_all_child_process(status) == PROCESS_ERROR)
+		return (PROCESS_ERROR);
+	ft_dprintf(STDERR_FILENO, "\nchild exit success!\n");
+	ft_dprintf(STDERR_FILENO, "status: %d, exit status: %d\n", \
+								WIFEXITED(status), last_exit_status);
 	return (EXIT_SUCCESS);
 }
