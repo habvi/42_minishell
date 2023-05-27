bac41cb50dc8b3bf5c61be3f43e9f09fc450d809
ak0327 <ca.fie.0220@gmail.com>
2023-05-27 21:36:53 +0900

    add(ft_legal_number) : implement ft_legal_number and add test in test/legal_number

--- includes/ft_builtin.h
+++ includes/ft_builtin.h
@@ -1,6 +1,8 @@
 #ifndef FT_BUILTIN_H
 # define FT_BUILTIN_H
 
+# include <stdbool.h>
+
 typedef enum e_exit_argument
 {
 	EXIT_VALID_ARG,
@@ -8,6 +10,12 @@ typedef enum e_exit_argument
 	NOT_EXIT_TOO_MANY_NUMERIC_ARG,
 }	t_exit_arg;
 
+<<<<<<< HEAD
 int	ft_echo(char **cmds);
+=======
+int		ft_echo(char **cmds);
+int		ft_exit(char **cmds);
+bool	ft_legal_number(const char *str, long *result);
+>>>>>>> 3f634aa (add(ft_legal_number):implement ft_legal_number and add simple test)
 
 #endif //FT_BUILTIN_H
