34adaf126ab2e1d2ad53f6e707f0bb399966d541
ak0327 <ca.fie.0220@gmail.com>
2023-05-27 10:26:59 +0900

    add(ft_exit): base files

--- includes/ft_builtin.h
+++ includes/ft_builtin.h
@@ -2,5 +2,6 @@
 # define FT_BUILTIN_H
 
 int	ft_echo(char **cmds);
+int	ft_exit(char **cmds);
 
 #endif //FT_BUILTIN_H
---
+++ srcs/builtin/ft_exit.c
@@ -0,0 +1,10 @@
+#include "ft_builtin.h"
+
+int	ft_exit(char **cmds)
+{
+	int	status;
+
+	(void)cmds;
+	status = EXIT_SUCCESS;
+	return (status);
+}
