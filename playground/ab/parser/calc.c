a0947d047c10c27b66445ce1c04b5bd0128770d5
habvi <93956895+habvi@users.noreply.github.com>
2023-05-28 22:53:47 +0200

    add(parse): calc from ast node

--- playground/ab/parser/calc.c
+++ playground/ab/parser/calc.c
@@ -1,7 +1,30 @@
 #include "test.h"
 
+static void	calc_each_node(t_ast_node *ast)
+{
+	if (ast->kind == NODE_ADD)
+		ast->val = ast->left->val + ast->right->val;
+	else if (ast->kind == NODE_SUB)
+		ast->val = ast->left->val - ast->right->val;
+	else if (ast->kind == NODE_MUL)
+		ast->val = ast->left->val * ast->right->val;
+	else if (ast->kind == NODE_DIV)
+		ast->val = ast->left->val / ast->right->val;
+	else
+		error_exit(__func__, "wrong operation...?");
+}
+
+static void	calc_ast_recursive(t_ast_node *ast)
+{
+	if (ast->kind == NODE_NUM)
+		return ;
+	calc_ast_recursive(ast->left);
+	calc_ast_recursive(ast->right);
+	calc_each_node(ast);
+}
+
 int	calc_from_ast(t_ast_node *ast)
 {
-	(void)ast;
-	return (0);
+	calc_ast_recursive(ast);
+	return (ast->val);
 }
