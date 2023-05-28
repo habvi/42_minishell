1733676ff98e2a75c6fc26bb5a92da7019f351fa
habvi <93956895+habvi@users.noreply.github.com>
2023-05-28 22:46:08 +0200

    update(parse): debug -> dprintf

--- playground/ab/parser/main.c
+++ playground/ab/parser/main.c
@@ -12,14 +12,17 @@ int	main(int argc, char **argv)
 		error_exit(__func__, "need 1 argv");
 	if (!ft_isdigit(argv[1][0]))
 		error_exit(__func__, "first argv must digit");
+
 	token = tokenize(argv[1]);
 	print_token(token);
+
 	token_head = token;
 	ast = create_ast(token);
-	print_ast(ast);
 	free_token(token_head); // free(token->str)
+	print_ast(ast);
+
 	result = calc_from_ast(ast);
-	printf("calc result: %d\n", result);
 	free_ast_node(ast);
+	printf("[calc result: %d]\n", result);
 	return (EXIT_SUCCESS);
 }
