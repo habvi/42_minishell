dbd75bfc4cf4edd35f2a92a668d6a4570ed4bf5e
habvi <93956895+habvi@users.noreply.github.com>
2023-05-28 21:58:11 +0200

    erase(parse): str in t_ast_node

--- playground/ab/parser/ast_node_new.c
+++ playground/ab/parser/ast_node_new.c
@@ -1,13 +1,13 @@
 #include "test.h"
 
-t_ast_node	*node_new(char *str, t_node_kind kind, t_ast_node *left, t_ast_node *right)
+t_ast_node	*node_new(t_node_kind kind, t_ast_node *left, t_ast_node *right)
 {
 	t_ast_node	*node;
 
+	printf("%s\n", __func__);
 	node = (t_ast_node *)malloc(sizeof(t_ast_node));
 	if (node == NULL)
 		error_exit(__func__, "failed to allocate");
-	node->str = str;
 	node->kind = kind;
 	node->val = 0;
 	node->left = left;
@@ -15,14 +15,14 @@ t_ast_node	*node_new(char *str, t_node_kind kind, t_ast_node *left, t_ast_node *
 	return (node);
 }
 
-t_ast_node	*node_num_new(char *str, int val)
+t_ast_node	*node_num_new(int val)
 {
 	t_ast_node	*node;
 
+	printf("%s: %d\n", __func__, val);
 	node = (t_ast_node *)malloc(sizeof(t_ast_node));
 	if (node == NULL)
 		error_exit(__func__, "failed to allocate");
-	node->str = str;
 	node->kind = NODE_NUM;
 	node->val = val;
 	node->left = NULL;
