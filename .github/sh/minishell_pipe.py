dd2be864125e3d027e5a795bdd6b9a1fa0b360d7
ak0327 <ca.fie.0220@gmail.com>
2023-05-29 12:30:11 +0900

    add(test py): eaad exit test

--- .github/sh/minishell_pipe.py
+++ .github/sh/minishell_pipe.py
@@ -293,6 +293,43 @@ def main():
     test_res |= test("ft_echo", echo_test)
 
 
+    exit_test = ["exit",
+                 "exit 0",
+                 "exit 1",
+                 "exit 42",
+                 "exit -1",
+                 "exit 2147483647",
+                 "exit -2147483648",
+                 "exit 9223372036854775807",
+                 "exit -9223372036854775808",
+                 "exit 0000000000000000000",
+                 "exit 0000000000000000001",
+                 # "exit \"  +42 \"",
+                 "exit 1 1",
+                 "exit -1 1",
+                 "exit 1 1 1",
+                 "exit 9223372036854775808",
+                 "exit a",
+                 "exit a b",
+                 "exit a b c",
+                 "exit a 1 b",
+                 "exit a 1 1",
+                 "exit a b 1",
+                 "exit --1",
+                 "exit -1+",
+                 "exit -1+ a",
+                 "exit -1+ 1",
+                 "exit 9999999999999999999999999999999999999999999999999999999999999999",
+                 "exit \t42",
+                 "exit \n42",
+                 "exit \v42",
+                 "exit \f42",
+                 "exit \r42",
+                 ] # add more test after update tokenizer
+
+    test_res |= test("ft_exit", exit_test)
+
+
     # stdin = "/bin/echo -e aaa\naacc\nbbb\nbbcc\nccc\naabb\nabc | /bin/grep a | /bin/grep c"
     # m_res, b_res = run_both(stdin)
     # put_result(val, m_res, b_res)
