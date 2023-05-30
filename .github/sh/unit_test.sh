5e6dd0f8cca6e1b8b4b22c92eb51e7d06a1a4a19
ak0327 <ca.fie.0220@gmail.com>
2023-05-30 20:16:03 +0900

    fix(unit_test): fix make ko count and refactor code.

--- .github/sh/unit_test.sh
+++ .github/sh/unit_test.sh
@@ -18,15 +18,19 @@ RESET='\033[0m'
 # func
 exec_make() {
     if "${MAKE_OUTPUT}"; then
-        make
+        make -C $1
     else
-        make >/dev/null 2>&1
+        make -C $1 >/dev/null 2>&1
     fi
 
     if [ $? -eq $MAKE_FAILURE ]; then
-        return $EXIT_FAILURE
+        make_res=$EXIT_FAILURE
+    else
+        make_res=$EXIT_SUCCESS
     fi
-    return $EXIT_SUCCESS
+
+    put_result $make_res " Make : "
+    return $make_res
 }
 
 exec_test() {
@@ -35,76 +39,91 @@ exec_test() {
     fi
 
     if "${TEST_OUTPUT}"; then
-        ./$PROGRAM_NAME
+        ./$2/$PROGRAM_NAME
     else
-        ./$PROGRAM_NAME >/dev/null 2>&1
+        ./$2/$PROGRAM_NAME >/dev/null 2>&1
     fi
 
-    return $?
+    test_res=$?
+    put_result $test_res  " Test : "
+    return $test_res
 }
 
 count_result() {
     if [ $1 -eq $EXIT_SUCCESS ]; then
         ((ok_cnt++))
-    else
-        ((ng_cnt++))
     fi
 }
 
 put_result() {
     if [ $1 -eq $EXIT_SUCCESS ]; then
-        echo -e "${GREEN}$2OK${RESET}"
+        echo -e "$2${GREEN}OK${RESET}"
     else
-        echo -e "${RED}$2NG${RESET}"
+        echo -e "$2${RED}NG${RESET}"
     fi
 }
 
 put_total_result() {
-    echo "Total tests : $total_cnt"
-    echo -e " ${GREEN}OK         : $ok_cnt${RESET}"
-    echo -e " ${RED}NG         : $ng_cnt${RESET}"
-    echo -e " ${YELLOW}SKIP       : $pass_cnt${RESET}"
+    echo " ------------------------------------"
+    echo " *           TOTAL RESULT           *"
+    echo " ------------------------------------"
+    echo -e "  ${GREEN}OK : $ok_cnt / $total_cnt${RESET}"
+
+    ng_cnt=$(($total_cnt - $ok_cnt))
+    if [ $ng_cnt -ne 0 ]; then
+        echo -e "  ${RED}NG : $ng_cnt / $total_cnt${RESET}"
+    fi
+    echo ""
+}
+
+is_dir_exist() {
+    if [ -d $1 ]; then
+        return 0
+    fi
+    return 1
+}
+
+is_makefile_exist() {
+    if [ -f "$dir"/Makefile ]; then
+        return 0
+    fi
+    return 1
+}
+
+if_make_success() {
+    if [ $status -eq $EXIT_SUCCESS ]; then
+        return 0
+    fi
+    return 1
 }
 
 # init
 total_cnt=0
 ok_cnt=0
-ng_cnt=0
-pass_cnt=0
 status=0
 
 cd $TEST_DIR
 
+echo " ------------------------------------"
+echo " *            TEST START            *"
+echo " ------------------------------------"
+
 for dir in *; do
-    status=$EXIT_SUCCESS
+    if ! is_dir_exist $dir; then
+        continue
+    fi
+
+    if ! is_makefile_exist $dir; then
+        continue
+    fi
     ((total_cnt++))
     echo -e "[TEST $total_cnt] $dir"
 #     echo -e "path:$PWD"
-    if [ -d "$dir" ]; then
-        cd $dir
-        if [ -f Makefile ]; then
-            exec_make $dir
-            status=$?
-            put_result $status " Make : "
 
-            if [ $status -ne $EXIT_SUCCESS ]; then
-                break
-            fi
-
-            exec_test $status
-            status=$?
-            put_result $status  " Test : "
-
-        else
-            echo -e " ${YELLOW}SKIP${RESET}"
-        fi
-
-        count_result $status
-        echo ""
-        cd ..
-    else
-        ((pass_cnt++))
-    fi
+    exec_make $dir
+    exec_test $? $dir
+    count_result $?
+    echo ""
 
 done
 
