dee7c4f1c02bab52ceb327c954075b7fd403b27e
habvi <93956895+habvi@users.noreply.github.com>
2023-06-08 07:58:14 +0200

    add: add_history ignore line

--- srcs/input/input.c
+++ srcs/input/input.c
@@ -1,6 +1,26 @@
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdbool.h>
 #include "minishell.h"
+
+static bool	is_empty(const char *line)
+{
+	return (line[0] == '\0');
+}
+
+static bool	is_ignorespace(const char *line)
+{
+	return (line[0] == ' ');
+}
+
+static void	add_accept_line_to_history(const char *line)
+{
+	if (is_empty(line))
+		return ;
+	if (is_ignorespace(line))
+		return ;
+	add_history(line);
+}
 
 char	*input_line(void)
 {
@@ -10,6 +30,6 @@ char	*input_line(void)
 	line = readline(PROMPT_NAME);
 	if (!line)
 		return (NULL);
-	add_history(line);
+	add_accept_line_to_history(line);
 	return (line);
 }
