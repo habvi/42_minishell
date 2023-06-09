3e7b7a524b321c1e4b4214184b5c8b9ca99681fa
habvi <93956895+habvi@users.noreply.github.com>
2023-06-09 18:21:22 +0200

    fmt: include category

--- includes/ms_exec.h
+++ includes/ms_exec.h
@@ -28,41 +28,41 @@ typedef struct s_fd {
 	int	prev_fd;
 }	t_fd;
 
-/* exec */
-// check_command.c
+/* check_command */
 bool			is_first_command(int prev_fd);
 bool			is_last_command(t_deque_node *next_cmd);
-// child_pipes.c
+
+/* child_process */
 int				handle_child_pipes(t_command *cmd, t_fd *fd);
-// child_proces.c
 void			child_process(t_command *cmd, \
 								t_fd *fd, \
 								char **environ, \
 								t_params *params);
-// exec.c
-int				execute_command(t_deque *dq_cmd, t_params *params);
-t_deque_node	*get_next_command(t_deque_node *cmd, size_t *cmd_size);
-char			**convert_command_to_array(t_deque_node *cmd, \
-											const size_t size);
 
-// init.c
-void			init_cmd(t_command *cmd, t_deque *dq_cmd);
-void			init_fd(t_fd *fd);
-// parent_pipes.c
-int				handle_parent_pipes(t_command *cmd, t_fd *fd);
-// parent_process.c
-int				parent_process(t_command *cmd, \
-								t_fd *fd, \
-								pid_t pid, \
-								int *last_exit_status);
+/* count_commands */
+size_t			count_commands(char *const *commands);
 
-// exec_builtin_in_parent_proc
+/* exec_builtin_in_parent_proc */
 int				call_builtin_command(char *const *command, t_params *params);
 int				exec_builtin_in_parent_proc(t_command cmd, \
 											t_deque_node *exec_cmd, \
 											t_params *params);
 
-// count_commands
-size_t			count_commands(char *const *commands);
+/* exec */
+int				execute_command(t_deque *dq_cmd, t_params *params);
+t_deque_node	*get_next_command(t_deque_node *cmd, size_t *cmd_size);
+char			**convert_command_to_array(t_deque_node *cmd, \
+											const size_t size);
+
+/* init */
+void			init_cmd(t_command *cmd, t_deque *dq_cmd);
+void			init_fd(t_fd *fd);
+
+/* parent */
+int				handle_parent_pipes(t_command *cmd, t_fd *fd);
+int				parent_process(t_command *cmd, \
+								t_fd *fd, \
+								pid_t pid, \
+								int *last_exit_status);
 
 #endif //MS_EXEC_H
