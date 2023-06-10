6096ea6720154300d79af3dd736db840812fbb08
habvi <93956895+habvi@users.noreply.github.com>
2023-06-10 20:43:41 +0200

    add(pop_selected): pop func

--- libft/srcs/ft_deque/dq_pop_selected_node.c
+++ libft/srcs/ft_deque/dq_pop_selected_node.c
@@ -1,10 +1,41 @@
 #include "ft_deque.h"
 
+static bool	is_head_node(t_deque *deque, t_deque_node *node)
+{
+	return (deque->node == node);
+}
+
+static bool	is_tail_node(t_deque *deque, t_deque_node *node)
+{
+	return (deque->node->prev == node);
+}
+
 static void	pop_target_node(t_deque *deque, t_deque_node *node)
 {
-	(void)deque;
-	(void)node;
+	t_deque_node	*left_node;
+	t_deque_node	*right_node;
+
+	if (deque->size == 1)
+	{
+		deque_pop_last_node(deque);
+		return ;
+	}
+	if (is_head_node(deque, node))
+		deque_pop_front(deque);
+	else if (is_tail_node(deque, node))
+		deque_pop_back(deque);
+	else
+	{
+		left_node = node->prev;
+		right_node = node->next;
+		deque_set_next(node, NULL);
+		deque_set_prev(node, NULL);
+		deque_set_next(left_node, right_node);
+		deque_set_prev(right_node, left_node);
+		deque->size--;
+	}
 }
+
 void	deque_pop_selected_node(t_deque *deque, t_deque_node *target_node)
 {
 	t_deque_node	*node;
