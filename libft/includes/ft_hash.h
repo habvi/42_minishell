9c82cdac865741409137ec03aa4490ac93f8a189
ak0327 <ca.fie.0220@gmail.com>
2023-06-11 10:12:00 +0900

    add(hash): add ft_hash.h and update libft.h

---
+++ libft/includes/ft_hash.h
@@ -0,0 +1,53 @@
+#ifndef FT_HASH_H
+#define FT_HASH_H
+
+# include <stddef.h>
+# include <stdint.h>
+
+# define HASH_SUCCESS	0
+# define HASH_ERROR		(-1)
+
+typedef struct s_deque	t_deque;
+
+typedef struct s_hash_table
+{
+	size_t	table_size;
+	size_t	key_count;
+	t_deque	**table;
+}	t_hash;
+
+typedef struct s_hash_element
+{
+	char	*key;
+	char	*value;
+}	t_elem;
+
+/* hash value */
+uint64_t	generate_fnv_hash_64(const unsigned char *key, uint64_t hash_mod);
+
+
+/* generate hash table */
+// return a pointer to the hash table. On error, return NULL
+t_hash		*create_hash_table(uint64_t size);
+
+/* add key */
+// add key-value pairs to table and return 0. On error, return (-1)
+// if hash_value conflicts, add with the chain method
+int			add_to_table(t_hash *hash, const char *elem_key, const char *elem_val);
+
+/* find key */
+
+/* get value */
+
+/* update value */
+
+/* del key */
+
+/* clear table */
+void		clear_hash_elem(t_elem **elem);
+void		clear_hash_table(t_hash **hash);
+
+/* display hash table */
+void		display_hash_table(t_hash *hash, void (*display)(void *));
+
+#endif //FT_HASH_H
