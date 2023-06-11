d44ad4b67f176ebf36e6f3132d756c9ab80876ef
habvi <93956895+habvi@users.noreply.github.com>
2023-06-11 09:46:00 +0200

    add(update_value): update_content_of_key

--- libft/srcs/ft_hash/update_value.c
+++ libft/srcs/ft_hash/update_value.c
@@ -1,9 +1,29 @@
+#include <stdlib.h>
+#include "ft_deque.h"
 #include "ft_hash.h"
+#include "ft_string.h"
 
-// free(key, pre-value), update value
+// key exist in hash
+// free(key, pre-content), update new content
 void	update_content_of_key(t_hash *hash, char *key, void *content)
 {
-	(void)hash;
-	(void)key;
-	(void)content;
+	uint64_t		hash_val;
+	t_deque_node	*node;
+	t_elem			*elem;
+
+	hash_val = generate_fnv_hash_64((unsigned char *)key, hash->table_size);
+	node = hash->table[hash_val]->node;
+	while (node)
+	{
+		elem = (t_elem *)node->content;
+		if (ft_streq(elem->key, key))
+		{
+			free(key);
+			// todo: del func
+			free(node->content);
+			node->content = content;
+			return ;
+		}
+		node = node->next;
+	}
 }
