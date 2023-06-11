e207b36120fe6a6e44f8e624a83f4f263c1049ce
ak0327 <ca.fie.0220@gmail.com>
2023-06-11 10:12:35 +0900

    add(hash): implement generate_hash() and add generate_hash.c

---
+++ libft/srcs/ft_hash/generate_hash.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include "ft_mem.h"
+
+//return hash
+uint64_t generate_fnv_hash_64(const unsigned char *key, uint64_t hash_mod)
+{
+	static const uint64_t	prime = 1099511628211LLU;
+	static const uint64_t	offset = 14695981039346656037LLU;
+	uint64_t				hash;
+	size_t					idx;
+
+	hash = offset;
+	if (!key)
+		return (hash);
+	idx = 0;
+	while (key[idx])
+	{
+		hash = (prime * hash) % hash_mod;
+		hash ^= key[idx];
+		idx++;
+	}
+	return (hash);
+}
