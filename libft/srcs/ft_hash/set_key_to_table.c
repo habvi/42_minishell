2cebc41bd4a3deaeef6484cf43c82e11181957ee
ak0327 <ca.fie.0220@gmail.com>
2023-06-12 12:21:01 +0900

    update(hash): overview of add_to_table

--- libft/srcs/ft_hash/set_key_to_table.c
+++ libft/srcs/ft_hash/set_key_to_table.c
@@ -37,6 +37,58 @@ static int	add_elem_to_table(t_hash *hash, t_elem *elem)
 	return (HASH_SUCCESS);
 }
 
+# define LOAD_FACTOR_PCT	80
+static bool	is_need_rehash(t_hash *hash)
+{
+	return (hash->key_count / hash->table_size * 100 >= LOAD_FACTOR_PCT);
+}
+
+int	rehash_table(t_hash *hash)
+{
+}
+
+static int	add_to_table(t_hash *hash, char *key, void *content)
+{
+	t_elem			*elem;
+	uint64_t		hash_val;
+	t_deque_node	*node;
+
+	// rehash
+	if (is_need_rehash(hash))
+	{
+		if (rehash_table(hash) == HASH_ERROR)
+			return (HASH_ERROR); // free hash by user
+	}
+
+	// gen hash
+	hash_val = generate_fnv_hash_64((const unsigned char *)key, hash->table_size);
+
+	// table[i]
+	if (!hash->table[hash_val])
+	{
+		hash->table[hash_val] = deque_new();
+		if (!hash->table[hash_val])
+			return (HASH_ERROR);
+	}
+
+	// create elem
+	elem = create_hash_elem(key, content);
+	if (!elem)
+		return (HASH_ERROR);
+
+	// add elem to table
+	if (add_elem_to_table(hash, elem) == HASH_ERROR)
+	{
+		clear_hash_elem(&elem, del);
+		return (HASH_ERROR);
+	}
+	node = deque_node_new(elem);
+	if (!node)
+		return (HASH_ERROR);
+	deque_add_back(hash->table[hash_val], node);
+	return (HASH_SUCCESS);
+}
+
 // if malloc error, return HASH_ERROR
 // hash not freed in func
 // 'key' cannot be null, 'value' can accept null
@@ -52,15 +104,8 @@ int	set_to_table(t_hash *hash, char *key, void *content)
 		update_content_of_key(&key, content, target_node);
 	else
 	{
-		elem = create_hash_elem(key, content);
-		if (!elem)
+		if (add_to_table(hash, key, content) == HASH_ERROR)
 			return (HASH_ERROR);
-		//todo:rehash
-		if (add_elem_to_table(hash, elem) == HASH_ERROR)
-		{
-			clear_hash_elem(&elem, del);
-			return (HASH_ERROR);
-		}
 		hash->key_count++;
 	}
 	return (HASH_SUCCESS);
