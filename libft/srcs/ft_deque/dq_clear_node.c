c91090e7f6f522110ddfd27afbe1a8e077ac302b
ak0327 <ca.fie.0220@gmail.com>
2023-06-13 17:10:24 +0900

    update(deque): add del as function argument

--- libft/srcs/ft_deque/dq_clear_node.c
+++ libft/srcs/ft_deque/dq_clear_node.c
@@ -1,11 +1,23 @@
 #include <stdlib.h>
 #include "ft_deque.h"
 
-void	deque_clear_node(t_deque_node **node)
+//void	deque_clear_node(t_deque_node **node)
+//{
+//	if (!*node)
+//		return ;
+//	free((*node)->content);
+//	(*node)->content = NULL;
+//	(*node)->next = NULL;
+//	(*node)->prev = NULL;
+//	free(*node);
+//	*node = NULL;
+//}
+
+void	deque_clear_node(t_deque_node **node, void (*del)(void *))
 {
 	if (!*node)
 		return ;
-	free((*node)->content);
+	del((*node)->content);
 	(*node)->content = NULL;
 	(*node)->next = NULL;
 	(*node)->prev = NULL;
