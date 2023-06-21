ea90a809df153880bda266963343909569b4bb1a
habvi <93956895+habvi@users.noreply.github.com>
2023-06-21 11:45:16 +0200

    add(env_set): imple env_set

--- srcs/environment/set.c
+++ srcs/environment/set.c
@@ -1,13 +1,29 @@
 #include "minishell.h"
 #include "ft_hash.h"
+#include "ft_mem.h"
+#include "ft_string.h"
 
-// key expected `name` -> void ...?
-int	env_set(t_env *env, char *key, char *value)
+// strdup key, value
+int	env_set(t_env *env, const char *key, const char *value)
 {
-	// strdup key, value
-	// set to hash
-	if (hs_set_key(env->hash, key, value) == HASH_ERROR)
-		return (FAILURE); // todo: hs_clear();
+	char	*dup_key;
+	char	*dup_value;
+
+	dup_key = ft_strdup(key);
+	if (!dup_key)
+		return (FAILURE);
+	dup_value = ft_strdup(value);
+	if (!dup_value)
+	{
+		ft_free(dup_key);
+		return (FAILURE);
+	}
+	if (hs_set_key(env->hash, dup_key, dup_value) == HASH_ERROR)
+	{
+		ft_free(dup_key);
+		ft_free(dup_value);
+		return (FAILURE);
+	}
 	return (SUCCESS);
 }
 
@@ -17,7 +33,6 @@ int	env_append(t_env *env, char *key, char *value)
 	// strdup key, value
 	// set to hash
 	if (hs_set_key(env->hash, key, value) == HASH_ERROR)
-		return (FAILURE); // todo: hs_clear();
+		return (FAILURE);
 	return (SUCCESS);
 }
-
