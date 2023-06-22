c67c0bfb54d0c93d3a8d136e988175d4190f7b0b
ak0327 <ca.fie.0220@gmail.com>
2023-06-23 01:25:33 +0900

    update(env): comment out

--- srcs/environment/init.c
+++ srcs/environment/init.c
@@ -30,26 +30,27 @@ static int	get_environ(t_env *env)
 
 	// todo
 	(void)environ;
+	(void)env;
 	// hs_add_to_table
 	// erase
-	hs_set_key(env->hash, ft_strdup("key_1"), ft_strdup("val_1"));
-	hs_set_key(env->hash, ft_strdup("key_2"), ft_strdup("val_2"));
-	hs_set_key(env->hash, ft_strdup("key_3"), ft_strdup("val_3"));
-	hs_set_key(env->hash, ft_strdup("key_4"), ft_strdup("val_4"));
+//	hs_set_key(env->hash, ft_strdup("key_1"), ft_strdup("val_1"));
+//	hs_set_key(env->hash, ft_strdup("key_2"), ft_strdup("val_2"));
+//	hs_set_key(env->hash, ft_strdup("key_3"), ft_strdup("val_3"));
+//	hs_set_key(env->hash, ft_strdup("key_4"), ft_strdup("val_4"));
 	return (SUCCESS);
 }
 
 // erase
-static void	display_elem(void *content)
-{
-	t_elem	*elem;
-
-	if (!content)
-		return ;
-	elem = content;
-	ft_dprintf(STDERR_FILENO, "[\"%s\", \"%s\"]", \
-	elem->key, (char *)elem->value);
-}
+//static void	display_elem(void *content)
+//{
+//	t_elem	*elem;
+//
+//	if (!content)
+//		return ;
+//	elem = content;
+//	ft_dprintf(STDERR_FILENO, "[\"%s\", \"%s\"]",
+//	elem->key, (char *)elem->value);
+//}
 
 static int	set_hash(t_env *env)
 {
@@ -64,7 +65,7 @@ static int	set_hash(t_env *env)
 		hs_clear(&env->hash);
 		return (FAILURE);
 	}
-	hs_display(env->hash, display_elem); // erase
+//	hs_display(env->hash, display_elem); // erase
 	return (SUCCESS);
 }
 
