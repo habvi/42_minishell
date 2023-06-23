2ea9cc8f6e72639b45f4154895c3e92cc97e5c22
ak0327 <ca.fie.0220@gmail.com>
2023-06-23 17:29:06 +0900

    fix(export): add return

--- srcs/builtin/ft_export.c
+++ srcs/builtin/ft_export.c
@@ -19,6 +19,7 @@ static int	declare_arg(const char *const arg, t_env *env, int *status)
 		*status = NOT_A_VALID_IDENTIFIER;
 		ft_dprintf(STDERR_FILENO, "%s: %s: %s: %s\n", \
 			SHELL_NAME, CMD_EXPORT, arg, ERROR_MSG_NOT_VALID_ID); // todo: func
+		return (SUCCESS);
 	}
 	if (result == CONTINUE)
 		return (SUCCESS);
