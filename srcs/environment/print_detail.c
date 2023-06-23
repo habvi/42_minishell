6d158cc5f3b145416c865bec84ae10449df3758a
ak0327 <ca.fie.0220@gmail.com>
2023-06-23 17:28:15 +0900

    add(env):implemetn print_detail

--- srcs/environment/print_detail.c
+++ srcs/environment/print_detail.c
@@ -1,32 +1,91 @@
 #include "minishell.h"
+#include "ms_builtin.h"
 #include "ft_deque.h"
 #include "ft_dprintf.h"
 #include "ft_hash.h"
+#include "ft_string.h"
 #include "ft_sys.h"
+#include "ft_mem.h"
 
-static void	set_elem_pointer(t_elem **elems, t_deque **table, const size_t size)
+static void	set_elem_pointer(t_elem **elems, \
+								t_deque **table, \
+								const size_t table_size)
 {
-	size_t			elem_i;
-	size_t			table_i;
+	size_t			i;
+	size_t			j;
 	t_deque_node	*node;
 
-	elem_i = 0;
-	table_i = 0;
-	while (table_i < size)
+	i = 0;
+	j = 0;
+	while (i < table_size)
 	{
-		if (!table[table_i])
+		if (!table[i])
 		{
-			table_i++;
+			i++;
 			continue ;
 		}
-		node = table[table_i]->node;
+		node = table[i]->node;
 		while (node)
 		{
-			elems[elem_i] = (t_elem *)node->content;
-			elem_i++;
+			elems[j] = (t_elem *)node->content;
+			j++;
 			node = node->next;
 		}
-		table_i++;
+		i++;
+	}
+	elems[j] = NULL;
+}
+
+static void	swap_elems(t_elem **elems, const size_t i, const size_t j)
+{
+	t_elem	*tmp;
+
+	tmp = elems[i];
+	elems[i] = elems[j];
+	elems[j] = tmp;
+}
+
+// sort to ascending
+// i <-> j
+static void	sort_elems_by_key(t_elem **elems)
+{
+	size_t	i;
+	size_t	j;
+	char	*key_i;
+	char	*key_j;
+
+	i = 0;
+	while (elems[i])
+	{
+		j = i;
+		while (elems[j])
+		{
+			key_i = elems[i]->key;
+			key_j = elems[j]->key;
+			if (i != j && ft_strcmp(key_i, key_j) > 0)
+				swap_elems(elems, i, j);
+			j++;
+		}
+		i++;
+	}
+}
+
+// declare -x KEY="VALUE"
+// declare -x KEY"
+static void	print_elems(t_elem **elems)
+{
+	size_t	i;
+
+	i = 0;
+	while (elems[i])
+	{
+		if (elems[i]->value)
+			ft_dprintf(STDOUT_FILENO, "%s %s=\"%s\"\n", \
+				DECLARE_X, elems[i]->key, elems[i]->value);
+		else
+			ft_dprintf(STDOUT_FILENO, "%s %s\n", \
+				DECLARE_X, elems[i]->key);
+		i++;
 	}
 }
 
@@ -37,12 +96,12 @@ int	env_print_detail(t_env *env)
 {
 	t_elem	**elems;
 
-	elems = (t_elem **)x_malloc(sizeof(t_elem *) * env->hash->key_count);
+	elems = (t_elem **)x_malloc(sizeof(t_elem *) * (env->hash->key_count + 1));
 	if (!elems)
 		return (PROCESS_ERROR);
 	set_elem_pointer(elems, env->hash->table, env->hash->table_size);
-	// sort
-	// print
-	// free elems
+	sort_elems_by_key(elems);
+	print_elems(elems);
+	free(elems);
 	return (SUCCESS);
 }
