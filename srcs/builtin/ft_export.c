ffa2cf8396e440a9f9511fdd277274ba79636dde
habvi <93956895+habvi@users.noreply.github.com>
2023-06-27 08:59:12 +0200

    update(export): for --

--- srcs/builtin/ft_export.c
+++ srcs/builtin/ft_export.c
@@ -23,34 +23,29 @@ static void	declare_all(const char *const *args, t_env *env, uint8_t *status)
 	}
 }
 
-static void	declare_to_env(const char *const *argv, t_env *env, uint8_t *status)
-{
-	if (is_option(argv[1]))
-	{
-		*status = INVALID_OPTION;
-		// todo: func
-		ft_dprintf(STDERR_FILENO, \
-				"%s: %s: %c%c: %s\n", \
-					SHELL_NAME, \
-					CMD_EXPORT, \
-					CMD_OPTION_MARKER, argv[1][1], \
-					ERROR_MSG_INVALID_OP);
-		return ;
-	}
-	declare_all(&argv[1], env, status);
-}
-
 uint8_t	ft_export(const char *const *argv, t_params *params)
 {
-	const size_t	argc = count_argv(argv);
-	uint8_t			status;
+	uint8_t	status;
+	size_t	i;
 
 	status = EXIT_SUCCESS;
-	if (argc == 1)
+	i = 1;
+	if (is_option(argv[i]))
+	{
+		status = INVALID_OPTION;
+		// todo: func
+		ft_dprintf(STDERR_FILENO, "%s: %s: %c%c: %s\n", SHELL_NAME, \
+					CMD_EXPORT, CMD_OPTION_MARKER, argv[i][1], \
+					ERROR_MSG_INVALID_OP);
+		return (status);
+	}
+	if (is_end_of_option(argv[i]))
+		i++;
+	if (!argv[i])
 	{
 		params->env->print_detail(params->env);
 		return (status);
 	}
-	declare_to_env(argv, params->env, &status);
+	declare_all(&argv[i], params->env, &status);
 	return (status);
 }
