8ce9e0ead81e875c4b79c18633f74840ab6ea1ba
habvi <93956895+habvi@users.noreply.github.com>
2023-06-28 18:11:10 +0200

    update(init_pwd): use env_set_dup_key_value()

--- srcs/environment/init_pwd.c
+++ srcs/environment/init_pwd.c
@@ -1,22 +1,17 @@
 #include "minishell.h"
 #include "ms_builtin.h"
-#include "ft_string.h"
+#include "ft_mem.h"
 
 void	init_pwd(t_env *env)
 {
 	char	*pwd_path;
-	char	*dup_key;
 
 	pwd_path = get_working_directory(SHELL_INIT);
 	if (pwd_path == NULL)
 		env->unset(env, KEY_PWD);
 	else
 	{
-		dup_key = ft_strdup(KEY_PWD);
-		if (!dup_key)
-			ft_abort();
-		if (!pwd_path)
-			ft_abort();
-		env->set(env, dup_key, pwd_path, ENV_ADD);
+		env_set_dup_key_value(env, KEY_PWD, pwd_path, ENV_ADD);
+		ft_free(&pwd_path);
 	}
 }
