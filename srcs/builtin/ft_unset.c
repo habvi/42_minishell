58fb3ee1edbb39e382444112532ea6b9882346b0
habvi <93956895+habvi@users.noreply.github.com>
2023-06-30 07:17:49 +0900

    fix(unset): can't handle internal?

--- srcs/builtin/ft_unset.c
+++ srcs/builtin/ft_unset.c
@@ -1,30 +1,16 @@
 #include <stdlib.h>
 #include "minishell.h"
 #include "ms_builtin.h"
-#include "ms_exec.h"
-#include "ft_mem.h"
-#include "ft_string.h"
 
-static void	unset_internal_old_pwd(t_context *context)
+static void	unset_args(const char *const *args, t_env *env, uint8_t *status)
 {
-	ft_free(&context->internal_old_pwd);
-}
-
-static void	unset_args(const char *const *args, t_context *context, uint8_t *status)
-{
-	t_env	*env;
 	size_t	i;
 
-	env = context->env;
 	i = 0;
 	while (args[i])
 	{
 		if (is_valid_key(args[i]))
-		{
 			env->unset(env, args[i]);
-			if (ft_streq(args[i], KEY_OLDPWD))
-				unset_internal_old_pwd(context);
-		}
 		else
 			*status = NOT_A_VALID_IDENTIFIER; // print error
 		i++;
@@ -40,6 +26,6 @@ uint8_t	ft_unset(const char *const *argv, t_context *context)
 	i = 1;
 	if (!is_valid_option(argv, &status, &i))
 		return (status);
-	unset_args(&argv[i], context, &status);
+	unset_args(&argv[i], context->env, &status);
 	return (status);
 }
