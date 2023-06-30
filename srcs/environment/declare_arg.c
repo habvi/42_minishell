3d9f6f5a4660c1a55c2cc81a5cd147a8182aca5e
ak0327 <ca.fie.0220@gmail.com>
2023-06-30 12:08:08 +0900

    add comment in env_devlare_arg

--- srcs/environment/declare_arg.c
+++ srcs/environment/declare_arg.c
@@ -12,7 +12,7 @@ t_result	env_declare_arg(const char *const arg, t_context *context)
 	result = env_separate_key_value(arg, &key, &value, &op);
 	if (result == FAILURE || result == CONTINUE)
 		return (result);
-	if (is_key_pwd_oldpwd(key))
+	if (is_key_pwd_oldpwd(key)) // todo: bool is_init ?
 		env_set_pwd_dup_key_value(context, key, &value, op);
 	else
 		env_set_dup_key_value(context->env, key, value, op);
