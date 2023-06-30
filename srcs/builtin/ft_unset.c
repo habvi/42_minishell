8fe36b118fe2d37180649ac828e891b1eaf8bc6b
ak0327 <ca.fie.0220@gmail.com>
2023-06-30 12:03:32 +0900

    update(unset): add unset internal var

--- srcs/builtin/ft_unset.c
+++ srcs/builtin/ft_unset.c
@@ -5,12 +5,19 @@
 #include "ft_mem.h"
 #include "ft_string.h"
 
+static void	unset_internal_pwd(t_context *context)
+{
+	ft_free(&context->internal_pwd);
+}
+
 static void	unset_internal_old_pwd(t_context *context)
 {
 	ft_free(&context->internal_old_pwd);
 }
 
-static void	unset_args(const char *const *args, t_context *context, uint8_t *status)
+static void	unset_args(const char *const *args, \
+						t_context *context, \
+						uint8_t *status)
 {
 	t_env	*env;
 	size_t	i;
@@ -22,7 +29,9 @@ static void	unset_args(const char *const *args, t_context *context, uint8_t *sta
 		if (is_valid_key(args[i]))
 		{
 			env->unset(env, args[i]);
-			if (ft_streq(args[i], KEY_OLDPWD))
+			if (ft_streq(args[i], KEY_PWD))
+				unset_internal_pwd(context);
+			else if (ft_streq(args[i], KEY_OLDPWD))
 				unset_internal_old_pwd(context);
 		}
 		else
