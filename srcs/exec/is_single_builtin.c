ec8e052f3ee183f917d28d5150ee388cf149e8ad
ak0327 <ca.fie.0220@gmail.com>
2023-07-01 17:20:41 +0900

    fix: fmt

--- srcs/exec/is_single_builtin.c
+++ srcs/exec/is_single_builtin.c
@@ -2,27 +2,26 @@
 #include "ft_deque.h"
 #include "ft_string.h"
 
-// todo: sort
 //echo, cd, pwd, export, unset, env, exit
 bool	is_command_builtin(const char *cmd)
 {
 	if (!cmd)
 		return (false);
-	if (ft_streq(cmd, CMD_ECHO))
-		return (true);
 	if (ft_streq(cmd, CMD_CD))
-		return (true);
-	if (ft_streq(cmd, CMD_PWD))
 		return (true);
 	if (ft_streq(cmd, CMD_DECLARE))
 		return (true);
-	if (ft_streq(cmd, CMD_EXPORT))
-		return (true);
-	if (ft_streq(cmd, CMD_UNSET))
+	if (ft_streq(cmd, CMD_ECHO))
 		return (true);
 	if (ft_streq(cmd, CMD_ENV))
 		return (true);
 	if (ft_streq(cmd, CMD_EXIT))
+		return (true);
+	if (ft_streq(cmd, CMD_EXPORT))
+		return (true);
+	if (ft_streq(cmd, CMD_PWD))
+		return (true);
+	if (ft_streq(cmd, CMD_UNSET))
 		return (true);
 	return (false);
 }
