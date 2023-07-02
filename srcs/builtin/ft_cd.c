9b2434f97ea0901f072a07a8a081baa1d561a0b7
ak0327 <ca.fie.0220@gmail.com>
2023-07-02 23:49:03 +0900

    add(cd): add update PWD, OLDPWD func

--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "minishell.h"
 #include "ms_builtin.h"
+#include "ms_var.h"
 #include "ft_mem.h"
 #include "ft_dprintf.h"
 
@@ -13,15 +14,14 @@ static bool	is_valid_cd_path(const char *path, int *tmp_err)
 	return (true);
 }
 
-void	cd_update_pwd(char *path, t_context *context)
+static void	update_internal_pwd(char *path, t_context *context)
 {
 	char	*absolute_path;
 
 	absolute_path = cd_canonicalize_path(path, context);
-	ft_free(&path);// todo: tmp
+	ft_free(&path);
 	ft_free(&context->internal_pwd);
 	context->internal_pwd = absolute_path;
-	// todo: iternal->pwd => update var->hash
 }
 
 void	update_pwd_in_cd(t_var *var, const char *key, const char *value, t_var_attr attr)
@@ -35,6 +35,41 @@ void	update_pwd_in_cd(t_var *var, const char *key, const char *value, t_var_attr
 	del_var_info((void **)&var_info);
 }
 
+static t_var_attr	get_attr_and_unset_if_newvalue_null(t_var *var)
+{
+	t_var_attr	attr;
+	char		*value;
+
+	attr = var_get_attribute(var, KEY_OLDPWD);
+	value = var_get_value(var, KEY_PWD);
+	if (!value)
+		var->unset(var, KEY_OLDPWD);
+	ft_free(&value);
+	return (attr);
+}
+
+static void	update_var_pwd_oldpwd(t_context *context)
+{
+	t_var		*var;
+	t_var_attr	oldpwd_attr;
+	char		*new_old_pwd;
+	char		*new_pwd;
+
+	var = context->var;
+	new_old_pwd = var_get_value(var, KEY_PWD);
+	new_pwd = context->internal_pwd;
+
+	oldpwd_attr = get_attr_and_unset_if_newvalue_null(var);
+	update_pwd_in_cd(var, KEY_OLDPWD, new_old_pwd, oldpwd_attr);
+	update_pwd_in_cd(var, KEY_PWD, new_pwd, VAR_NONE);
+	ft_free(&new_old_pwd);
+}
+
+void	cd_update_pwd(char *path, t_context *context)
+{
+	update_internal_pwd(path, context);
+	update_var_pwd_oldpwd(context);
+}
 
 static void	change_directory(const char *arg, \
 								t_context *context, \
