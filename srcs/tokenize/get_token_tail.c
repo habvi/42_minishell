c36280c0a2c63b1936c00ba7502c0659cf469cb5
ak0327 <ca.fie.0220@gmail.com>
2023-07-04 01:12:51 +0900

    add(token): get_token_tail()

---
+++ srcs/tokenize/get_token_tail.c
@@ -0,0 +1,42 @@
+#include "minishell.h"
+#include "ms_tokenize.h"
+#include "ms_builtin.h"
+#include "ft_mem.h"
+#include "ft_string.h"
+
+static char	*get_token_symbol_tail(char *head, char symbol)
+{
+	while (*head && *head == symbol)
+		head++;
+	return (head);
+}
+
+static bool	is_token_str_symbol(char *set, char chr)
+{
+	return (ft_strchr(set, chr));
+}
+
+static bool	is_token_str_quote(char *set, char chr)
+{
+	return (ft_strchr(set, chr));
+}
+
+// 3 patterns: token str is 1)symbol, 2)quoted, 3)word
+char	*get_token_tail(char *head)
+{
+	char		*tail;
+	const char	head_chr = *head;
+
+	head++;
+	if (is_token_str_symbol(TOKEN_SYMBOL, head_chr))
+		tail = get_token_symbol_tail(head, head_chr);
+	else if (is_token_str_quote(TOKEN_QUOTE, head_chr))
+	{
+		tail = ft_find_set_in_str(head, (char [2]){head_chr, 0});
+		if (*tail)
+			tail++;
+	}
+	else
+		tail = ft_find_set_in_str(head, TOKEN_SYMBOL TOKEN_QUOTE TOKEN_DELIM);
+	return (tail);
+}
