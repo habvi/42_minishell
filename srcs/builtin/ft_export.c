3e667dc8bbdcb2fefb886f2b4528b40339d9fdd9
ak0327 <ca.fie.0220@gmail.com>
2023-07-03 11:16:10 +0900

    mv declare all()

--- srcs/builtin/ft_export.c
+++ srcs/builtin/ft_export.c
@@ -1,33 +1,8 @@
 #include <stdlib.h>
-#include "minishell.h"
 #include "ms_builtin.h"
 #include "ms_var.h"
-#include "ft_dprintf.h"
 
 // export declare VAR_ENV
-void	declare_all(const char *const *args, \
-					t_var *var, \
-					uint8_t *status, \
-					t_var_attr attr)
-{
-	size_t		i;
-	t_result	result;
-
-	i = 0;
-	while (args[i])
-	{
-		result = var_declare_arg(args[i], var, attr);
-		if (result == FAILURE)
-		{
-			*status = NOT_A_VALID_IDENTIFIER;
-			// todo: func
-			ft_dprintf(STDERR_FILENO, "%s: %s: %s: %s\n", \
-				SHELL_NAME, CMD_EXPORT, args[i], ERROR_MSG_NOT_VALID_ID);
-		}
-		i++;
-	}
-}
-
 uint8_t	ft_export(const char *const *argv, t_var *var)
 {
 	uint8_t	status;
