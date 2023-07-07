585a19c695a651738f99ad2b2d79cda1d167b648
habvi <93956895+habvi@users.noreply.github.com>
2023-07-07 19:31:59 +0900

    rm(repl): destroy tokens, delete unnecessary func

--- srcs/repl.c
+++ srcs/repl.c
@@ -6,49 +6,6 @@
 #include "ft_deque.h"
 #include "ft_mem.h"
 
-void	destroy_tmp(t_deque **command, void (*del_token)(void *), t_ast *ast)
-{
-	if (command)
-		destroy_tokens(command, del_token);
-	if (ast)
-		destroy_ast_tree(&ast);
-}
-
-// todo: after parser done, erase this func.
-// deque_node->content : only (char *)t_token->str
-static t_deque	*tmp_func_convert_to_executable_command(t_deque *tmp_cmds)
-{
-	t_deque			*cmd;
-	t_deque_node	*tmp_node;
-	t_token			*token;
-	char			*dup_str;
-	t_deque_node	*new_node;
-
-	cmd = deque_new();
-	if (!cmd)
-		ft_abort();
-	tmp_node = tmp_cmds->node;
-	while (tmp_node)
-	{
-		token = (t_token *)tmp_node->content;
-		dup_str = x_ft_strdup(token->str);
-		new_node = deque_node_new((void *)dup_str);
-		deque_add_back(cmd, new_node);
-		tmp_node = tmp_node->next;
-	}
-	return (cmd);
-}
-
-static t_deque	*tmp_convert(t_deque **tokens)
-{
-	t_deque		*command;
-
-	command = tmp_func_convert_to_executable_command(*tokens);
-	if (*tokens)
-		destroy_tokens(tokens, del_token);
-	return (command);
-}
-
 t_result	read_eval_print_loop(t_context *context)
 {
 	t_deque		*command;
@@ -69,9 +26,8 @@ t_result	read_eval_print_loop(t_context *context)
 		ast = parse(&tokens, context);
 		if (context->status != EXIT_SUCCESS)
 			continue ;
-		command = tmp_convert(&tokens);
-		result = execute_command(command, context);
-		destroy_tmp(&command, free, ast);
+		result = execute_command(ast, context);
+		destroy_ast_tree(&ast);
 		if (result == PROCESS_ERROR)
 			break ;
 	}
