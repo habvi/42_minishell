fc6a99e14a8c8cc5d6b091dfe01920df29fcb282
habvi <93956895+habvi@users.noreply.github.com>
2023-07-10 23:24:16 +0900

    update(cd): use is_print_path var

--- srcs/builtin/ft_cd/ft_cd_set_path.c
+++ srcs/builtin/ft_cd/ft_cd_set_path.c
@@ -6,20 +6,19 @@
 // "~"  -> HOME
 // "-"  -> OLDPWD
 // else -> arg
-static char	*set_path_by_arg(const char *arg, t_var *var)
+static char	*set_path_by_arg(const char *arg, t_var *var, bool *is_print_path)
 {
 	char	*path;
 
 	if (ft_streq(arg, CD_ARG_HOME)) // ~ todo: expansion
 		path = var->get_value(var, KEY_HOME);
 	else if (ft_streq(arg, CD_ARG_OLDPWD)) // -
+	{
+		*is_print_path = true;
 		path = var->get_value(var, KEY_OLDPWD);
+	}
 	else
-		// search CDPATH
-		// exist
-			// path => absolute
-		// else
-		path = x_ft_strdup(arg);
+		path = search_cdpath(arg, var, is_print_path);
 	return (path);
 }
 
@@ -27,13 +26,14 @@ static char	*set_path_by_arg(const char *arg, t_var *var)
 // unset HOME; cd <no arg>   path=NULL; HOME not set
 // unset HOME; cd ~          getpath from /etc/passwd   (tmp) same as <no arg>
 // -> unset OLD ;cd -        path=NULL; OLDPWD not set
-char	*cd_set_path(const char *arg, t_var *var)
+char	*cd_set_path(const char *arg, t_var *var, bool *is_print_path)
 {
 	char	*path;
 
+	*is_print_path = false;
 	if (!arg)
 		path = var->get_value(var, KEY_HOME);
 	else
-		path = set_path_by_arg(arg, var);
+		path = set_path_by_arg(arg, var, is_print_path);
 	return (path);
 }
