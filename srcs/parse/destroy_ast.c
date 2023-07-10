a6a5177d89545d623736f95f6c415fa4d13f8973
ak0327 <ca.fie.0220@gmail.com>
2023-07-10 17:04:08 +0900

    update(minishell): handle defunct process if PROCESS_ERROR

--- srcs/parse/destroy_ast.c
+++ srcs/parse/destroy_ast.c
@@ -30,9 +30,21 @@ static void	destroy_ast_node(t_ast *root)
 	ft_free(&root);
 }
 
-void	destroy_ast_tree(t_ast **root)
+static void	wait_all_child_lastly(t_ast *ast)
+{
+	if (!ast)
+		return ;
+	wait_all_child_lastly(ast->left);
+	wait_all_child_lastly(ast->right);
+	if (ast->pid)
+		waitpid(ast->pid, NULL, 0);
+}
+
+void	destroy_ast_tree(t_ast **root, t_result result)
 {
 	if (!root || !*root)
 		return ;
+	if (result == PROCESS_ERROR)
+		wait_all_child_lastly(*root);
 	destroy_ast_node(*root);
 }
