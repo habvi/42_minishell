484d5a78e59f2b52df3b6b98a34c2b3fb23f3dd8
ak0327 <ca.fie.0220@gmail.com>
2023-07-11 15:11:54 +0900

    add test and fix error msg

--- test/integration_test/run_export.py
+++ test/integration_test/run_export.py
@@ -1,25 +1,26 @@
 from test_functions import test
 
+# PRINT_CMD = "export | grep -v ^_ | grep -v SHLVL | grep -v PS1 | grep -v MAKE_TERMERR | grep -v MAKE_TERMOUT | grep -v ^$ | tr -d \'\\\'"
+PRINT_CMD = "export | grep -v ^_ | grep -v SHLVL | grep -v PS1 | grep -v MAKE_TERMERR | grep -v MAKE_TERMOUT"
 
 def main():
     test_res = 0
     echo_test = ["export A1=a1",
-                 "env | grep -v ^_ | sort",  # tmp
+                 PRINT_CMD,
                  "export B1=b1 B2=b2",
-                 "env | grep -v ^_ | sort",  # tmp
+                 PRINT_CMD,
                  "export A1+=a1",
                  "export A2+=a2",
-                 "env | grep -v ^_ | sort",  # tmp
+                 PRINT_CMD,
                  "export C1=c1 C1 C1=",
                  "export _",
                  "export _=",
                  "export _=aaaa",
                  "export __=bbbb",
                  "export OK1=ok1 -NG=ng OK2=ok2",
-                 "env | grep -v ^_ | sort",  # tmp
-                 "export -option KEU=value",
-                 "env | grep -v ^_ | sort",  # tmp
-                 "unset A1",
+                 PRINT_CMD,
+                 # "export -option KEU=value",
+                 # PRINT_CMD,
                  ]
 
     test_res |= test("ft_export", echo_test)
