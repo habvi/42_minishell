cea3b3c3984b1a85a4ea412dac25d9ffe1cd0a79
habvi <93956895+habvi@users.noreply.github.com>
2023-07-11 15:47:19 +0900

    rm: unnecessary head_node param

--- srcs/parse/parse.c
+++ srcs/parse/parse.c
@@ -2,22 +2,22 @@
 #include "ms_parse.h"
 #include "ms_tokenize.h"
 #include "ft_deque.h"
-#include "ft_dprintf.h"
 
-static t_ast	*create_ast(t_deque_node **head_node, \
-							t_deque **tokens, \
+static t_ast	*create_ast(t_deque **tokens, \
 							t_context *context, \
 							t_result *result)
 {
-	t_ast	*ast;
+	t_deque_node	*head_node;
+	t_ast			*ast;
 
-	ast = create_operator_list_node(head_node, context);
-	if (*head_node)
+	head_node = (*tokens)->node;
+	ast = create_operator_list_node(&head_node, context);
+	if (head_node)
 		set_error_status(context, result);
 	if (context->status == SYNTAX_ERROR)
 	{
 		set_error_status(context, result);
-		ast_print_error(*head_node);
+		ast_print_error(head_node);
 		destroy_tokens(tokens, del_token);
 		destroy_ast_tree(&ast, *result);
 		return (NULL);
@@ -29,16 +29,14 @@ static t_ast	*create_ast(t_deque_node **head_node, \
 // always free tokens
 t_ast	*parse(t_deque **tokens, t_context *context, t_result *result)
 {
-	t_ast			*ast;
-	t_deque_node	*head_node;
-	t_result		heredoc_result;
+	t_ast		*ast;
+	t_result	heredoc_result;
 
 	if (deque_is_empty(*tokens))
 		return (destroy_tokens(tokens, del_token));
-	head_node = (*tokens)->node;
-	if (!is_valid_pre_parse_syntax(head_node, tokens, context, result))
+	if (!is_valid_pre_parse_syntax(tokens, context, result))
 		return (NULL);
-	ast = create_ast(&head_node, tokens, context, result);
+	ast = create_ast(tokens, context, result);
 	if (!ast)
 		return (NULL);
 	heredoc_result = execute_heredoc(ast);
--- srcs/parse/pre_parse_syntax.c
+++ srcs/parse/pre_parse_syntax.c
@@ -21,11 +21,13 @@ static bool	is_valid_pre_parse_syntax_inter(t_deque_node *node)
 	return (true);
 }
 
-bool	is_valid_pre_parse_syntax(t_deque_node *head_node, \
-									t_deque **tokens, \
+bool	is_valid_pre_parse_syntax(t_deque **tokens, \
 									t_context *context, \
 									t_result *result)
 {
+	t_deque_node	*head_node;
+
+	head_node = (*tokens)->node;
 	if (!is_valid_pre_parse_syntax_inter(head_node))
 	{
 		set_error_status(context, result);
