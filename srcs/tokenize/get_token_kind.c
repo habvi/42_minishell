aa796f8dd252cc906b5aa014eb70ee2c6b6ad064
ak0327 <ca.fie.0220@gmail.com>
2023-07-13 18:31:51 +0900

    add(token): sep get_token_kind() to new file

---
+++ srcs/tokenize/get_token_kind.c
@@ -0,0 +1,10 @@
+#include "ms_parse.h"
+#include "ms_tokenize.h"
+#include "ft_deque.h"
+
+t_token_kind	get_token_kind(t_deque_node *token_node)
+{
+	const t_token	*token = (t_token *)token_node->content;
+
+	return (token->kind);
+}
