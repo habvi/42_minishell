c0edfcbff27bb589e8104b8fefe46128f9bd9996
ak0327 <ca.fie.0220@gmail.com>
2023-07-14 15:53:31 +0900

    update(gnl): norm

--- libft/includes/ft_gnl_inter.h
+++ libft/includes/ft_gnl_inter.h
@@ -1,5 +1,5 @@
-#ifndef GET_NEXT_LINE_INTER_H
-# define GET_NEXT_LINE_INTER_H
+#ifndef FT_GNL_INTER_H
+# define FT_GNL_INTER_H
 
 # include <stdlib.h> // malloc, free
 # include <unistd.h> // read
@@ -10,10 +10,9 @@
 # define BUFFER_SIZE    1024
 # define MY_OPEN_MAX    1024
 # define LF             '\n'
-# define GNL_READ_ERROR -1
+# define GNL_READ_ERROR (-1)
 
 // get_next_line_utils.c
-char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr_for_gnl(char const *s, unsigned int start, size_t len);
 void	*ft_memmove_for_gnl(void *dst, const void *src, size_t len);
 
