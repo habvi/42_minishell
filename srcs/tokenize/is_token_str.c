427913630955780680da77bf4cb49c6186da7376
ak0327 <ca.fie.0220@gmail.com>
2023-07-15 16:52:51 +0900

    update(review)

--- srcs/tokenize/is_token_str.c
+++ srcs/tokenize/is_token_str.c
@@ -5,7 +5,7 @@ bool	is_token_str_symbol(const char *str)
 {
 	const char	head_chr = *str;
 
-	if (!(ft_strlen(str) && ft_strchr(TOKEN_SYMBOL, head_chr)))
+	if (!(str && str[0] && ft_strchr(TOKEN_SYMBOL, head_chr)))
 		return (false);
 	if (str[0] == AND_CHR && str[1] != AND_CHR)
 		return (false);
@@ -16,12 +16,12 @@ bool	is_token_str_quote(const char *str)
 {
 	const char	head_chr = *str;
 
-	return (ft_strlen(str) && ft_strchr(TOKEN_QUOTE, head_chr));
+	return (str && str[0] && ft_strchr(TOKEN_QUOTE, head_chr));
 }
 
 bool	is_token_str_paren(const char *str)
 {
 	const char	head_chr = *str;
 
-	return (ft_strlen(str) && ft_strchr(TOKEN_PAREN, head_chr));
+	return (str && str[0] && ft_strchr(TOKEN_PAREN, head_chr));
 }
