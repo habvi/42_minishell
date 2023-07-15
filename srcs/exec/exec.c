56a3e43647b51e930e095b40b5876f21acbf3f4c
ak0327 <ca.fie.0220@gmail.com>
2023-07-15 12:12:21 +0900

    fix(exec): receive result from expand_variables()

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -52,7 +52,9 @@ static t_result	execute_command_internal(t_ast *self_node, t_context *context)
 	result = copy_stdio_fd(&stdin_copy, &stdout_copy);
 	if (result == PROCESS_ERROR)
 		return (PROCESS_ERROR);
-	expand_variables(self_node, context);
+	result = expand_variables(self_node, context);
+	if (result == PROCESS_ERROR)
+		return (PROCESS_ERROR);
 	result = redirect_fd(self_node, context);
 	if ((result == PROCESS_ERROR) || (result == FAILURE))
 		return (result);
