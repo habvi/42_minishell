7f6a4ae792e63a4d94e8bc5543c85fcc6f3d1ead
ak0327 <ca.fie.0220@gmail.com>
2023-07-15 14:04:12 +0900

    fix(token): mystery concat paren error, ex) $ ''a

--- srcs/tokenize/is_token_str.c
+++ srcs/tokenize/is_token_str.c
@@ -6,19 +6,19 @@ bool	is_token_str_symbol(const char *str)
 {
 	const char	head_chr = *str;
 
-	return (ft_strchr(TOKEN_SYMBOL, head_chr));
+	return (ft_strlen(str) && ft_strchr(TOKEN_SYMBOL, head_chr));
 }
 
 bool	is_token_str_quote(const char *str)
 {
 	const char	head_chr = *str;
 
-	return (ft_strchr(TOKEN_QUOTE, head_chr));
+	return (ft_strlen(str) && ft_strchr(TOKEN_QUOTE, head_chr));
 }
 
 bool	is_token_str_paren(const char *str)
 {
 	const char	head_chr = *str;
 
-	return (ft_strchr(TOKEN_PAREN, head_chr));
+	return (ft_strlen(str) && ft_strchr(TOKEN_PAREN, head_chr));
 }
