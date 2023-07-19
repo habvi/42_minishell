09c0a9ab4cbe40a63dd4fee3290d53f13b574218
habvi <93956895+habvi@users.noreply.github.com>
2023-07-20 00:18:17 +0900

    update(chdir_absolute): if chdir() failed, print error once

--- srcs/builtin/ft_cd/chdir_absolute.c
+++ srcs/builtin/ft_cd/chdir_absolute.c
@@ -14,11 +14,8 @@ t_result	cd_chdir_from_absolute_path(const char *path, char **absolute_path)
 	result = cd_change_dir_to_valid_path(path, &tmp_err);
 	if (result == FAILURE)
 	{
-		if (tmp_err == EACCES)
-		{
-			puterr_cmd_arg_msg(CMD_CD, path, strerror(tmp_err));
-			// ft_dprintf(2, "%d: [%s]\n", __LINE__, *absolute_path);
-		}
+		puterr_cmd_arg_msg(CMD_CD, path, strerror(tmp_err));
+		// ft_dprintf(2, "%d: [%s]\n", __LINE__, *absolute_path);
 		return (FAILURE);
 	}
 	ft_free(absolute_path);
