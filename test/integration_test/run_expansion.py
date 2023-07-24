1c82631b543d17c60293b450144a7a6c69c2053e
habvi <93956895+habvi@users.noreply.github.com>
2023-07-24 20:43:13 +0900

    add: test wildcard

--- test/integration_test/run_expansion.py
+++ test/integration_test/run_expansion.py
@@ -1,5 +1,10 @@
 from test_function.test_functions import test
 
+# for wildcard
+MAKE_TEST_DIR = "mkdir -p test_wildcard && cd test_wildcard && "
+RANDOM_FILES = []
+TOUCH_TEST_Z = f"{MAKE_TEST_DIR} touch z1 zz2 3z 4zz zz5z z6zz z7z7z z8zz8zz zz9z9zzz"
+SORT_COMPARE = " | tr ' ' '\\n' | sort && rm -rf test_wildcard"
 
 def main():
     test_res = 0
@@ -9,7 +14,26 @@ def main():
                     ]
 
     wildcard_test = [
-
+                    f"echo * | {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo * {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo z* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo *z {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo *z* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo **z* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo *z** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo **z** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo *nosuch {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo nosuch* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo *nosuch* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo **nosuch* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo *nosuch** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo **nosuch** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo * * {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo * z* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo z* * {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo z* *z {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo * z* *z {SORT_COMPARE}",
+                    f"{TOUCH_TEST_Z} echo * z* *nosuch *z {SORT_COMPARE}",
                     ]
 
     expansion_test = [
@@ -49,8 +73,8 @@ def main():
                     ] # todo more test
 
 
-    # test_res |= test("word_splitting", word_splitting_test, False)
-    # test_res |= test("wildcard", wildcard_test, False)
+    # test_res |= test("word_splitting", word_splitting_test, False, False)
+    test_res |= test("wildcard", wildcard_test, False, False)
     test_res |= test("expansion", expansion_test, False, False)
 
     return test_res
