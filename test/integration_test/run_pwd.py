c915c9418132b6b3da4084fd2addb7eef21ab40c
ak0327 <ca.fie.0220@gmail.com>
2023-07-25 00:11:29 +0900

    update(test)

--- test/integration_test/run_pwd.py
+++ test/integration_test/run_pwd.py
@@ -3,8 +3,8 @@ from test_function.test_functions import test
 MKDIR = "rm -rf test_dir \n mkdir test_dir \n cd test_dir"
 RMDIR = "cd .. \n chmod 777 test_dir \n rm -rf test_dir"
 PATH_ADD = "export PATH+=:$PWD"
-SHOW_PWD = "\necho 'pwd: '\n pwd \necho status: \"$?\"\n declare -p | grep PWD\n"
-SHELL_PWD = "\n__SHELL__\n echo 'pwd: '\n pwd \necho status: \"$?\"\n declare -p | grep PWD\n"
+SHOW_PWD =              "\necho 'pwd: '          \n pwd \necho status: \"$?\"\n declare -p | grep PWD\n"
+SHELL_PWD = "\n__SHELL__\n echo 'new shell pwd: '\n pwd \necho status: \"$?\"\n declare -p | grep PWD\n"
 
 def main():
 
@@ -122,6 +122,115 @@ def main():
         f"{PATH_ADD}\n unset PWD        \n {SHELL_PWD * 10}",
         f"{PATH_ADD}\n export PWD=xxxx  \n {SHELL_PWD * 10}\n export PWD=yyyy \n {SHELL_PWD * 10}",
         f"{PATH_ADD}\n export PWD=xxxx  \n {SHELL_PWD * 10}\n unset PWD \n {SHELL_PWD * 10}",
+
+
+        f"echo 1-1-1 current        \n {PATH_ADD}\n                     {SHOW_PWD} ",
+        f"echo 1-1-2 current        \n {PATH_ADD}\n unset  PWD       \n {SHOW_PWD} ",
+        f"echo 1-1-3 current        \n {PATH_ADD}\n export PWD=xxxxx \n {SHOW_PWD} ",
+        f"echo 1-1-4 current        \n {PATH_ADD}\n export PWD=$HOME \n {SHOW_PWD} ",
+        f"echo 1-1-5 current        \n {PATH_ADD}\n export PWD=/     \n {SHOW_PWD} ",
+        f"echo 1-1-6 current        \n {PATH_ADD}\n export PWD=//    \n {SHOW_PWD} ",
+        f"echo 1-1-7 current        \n {PATH_ADD}\n export PWD=.     \n {SHOW_PWD} ",
+        f"echo 1-1-8 current        \n {PATH_ADD}\n export PWD=..    \n {SHOW_PWD} ",
+        f"echo 1-1-9 current        \n {PATH_ADD}\n export PWD=../.. \n {SHOW_PWD} ",
+        f"echo 1-1-10 current        \n {PATH_ADD}\n export PWD=../../../../../ \n {SHOW_PWD} ",
+        f"echo 1-1-11 current        \n {PATH_ADD}\n cd /             \n {SHOW_PWD} ",
+        f"echo 1-1-12 current        \n {PATH_ADD}\n cd //            \n {SHOW_PWD} ",
+        f"echo 1-1-13 current        \n {PATH_ADD}\n cd ///           \n {SHOW_PWD} ",
+        f"echo 1-1-14 current        \n {PATH_ADD}\n cd .             \n {SHOW_PWD} ",
+        f"echo 1-1-15 current        \n {PATH_ADD}\n cd ..            \n {SHOW_PWD} ",
+        f"echo 1-1-16 current        \n {PATH_ADD}\n cd ../../        \n {SHOW_PWD} ",
+        f"echo 1-1-17 current        \n {PATH_ADD}\n cd ../../../../../\n {SHOW_PWD} ",
+
+        f"echo 1-1-12 current        \n {PATH_ADD}\n cd //            \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-13 current        \n {PATH_ADD}\n cd ///           \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-14 current        \n {PATH_ADD}\n cd .             \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-15 current        \n {PATH_ADD}\n cd ..            \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-16 current        \n {PATH_ADD}\n cd ../../        \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-17 current        \n {PATH_ADD}\n cd ../../../../../\n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+
+        f"echo 1-1-11 current        \n {PATH_ADD}\n cd /             \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-12 current        \n {PATH_ADD}\n cd //            \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-13 current        \n {PATH_ADD}\n cd ///           \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-14 current        \n {PATH_ADD}\n cd .             \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-15 current        \n {PATH_ADD}\n cd ..            \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-16 current        \n {PATH_ADD}\n cd ../../        \n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-17 current        \n {PATH_ADD}\n cd ../../../../../\n export PWD=xxxxx \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+
+
+        f"echo 1-1-11 current        \n {PATH_ADD}\n cd /             \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-12 current        \n {PATH_ADD}\n cd //            \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-13 current        \n {PATH_ADD}\n cd ///           \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-14 current        \n {PATH_ADD}\n cd .             \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-15 current        \n {PATH_ADD}\n cd ..            \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-16 current        \n {PATH_ADD}\n cd ../../        \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-17 current        \n {PATH_ADD}\n cd ../../../../../\n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+
+        f"echo 1-1-11 current        \n {PATH_ADD}\n cd /             \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-12 current        \n {PATH_ADD}\n cd //            \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-13 current        \n {PATH_ADD}\n cd ///           \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-14 current        \n {PATH_ADD}\n cd .             \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-15 current        \n {PATH_ADD}\n cd ..            \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-16 current        \n {PATH_ADD}\n cd ../../        \n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+        f"echo 1-1-17 current        \n {PATH_ADD}\n cd ../../../../../\n unset PWD \n {SHOW_PWD}\n __SHELL__ \n {SHOW_PWD} ",
+
+        f"echo 1-2-1 current new    \n {PATH_ADD}\n                     {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-2 current new    \n {PATH_ADD}\n unset  PWD       \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-3 current new    \n {PATH_ADD}\n export PWD=xxxxx \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-4 current new    \n {PATH_ADD}\n export PWD=$HOME \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-5 current new    \n {PATH_ADD}\n export PWD=/     \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-6 current new    \n {PATH_ADD}\n export PWD=//    \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-7 current new    \n {PATH_ADD}\n export PWD=.     \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-8 current new    \n {PATH_ADD}\n export PWD=..    \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-9 current new    \n {PATH_ADD}\n export PWD=../.. \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-2-10 current new    \n {PATH_ADD}\n export PWD=../../../../../ \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-1-11 current new    \n {PATH_ADD}\n cd /             \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-1-12 current new    \n {PATH_ADD}\n cd //            \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-1-13 current new    \n {PATH_ADD}\n cd ///           \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-1-14 current new    \n {PATH_ADD}\n cd .             \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-1-15 current new    \n {PATH_ADD}\n cd ..            \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-1-16 current new    \n {PATH_ADD}\n cd ../../        \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 1-1-17 current new    \n {PATH_ADD}\n cd ../../../../../\n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+
+
+        f"echo 2-2-1 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n                     {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-2 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n unset  PWD       \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-3 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n export PWD=xxxxx \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-4 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n export PWD=$HOME \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-5 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n export PWD=/     \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-6 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n export PWD=//    \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-7 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n export PWD=.     \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-8 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n export PWD=..    \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-9 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n export PWD=../.. \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-2-10 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n export PWD=../../../../../ \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-1-11 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n cd /             \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-1-12 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n cd //            \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-1-13 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n cd ///           \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-1-14 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n cd .             \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-1-15 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n cd ..            \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-1-16 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n cd ../../        \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-1-17 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n cd ../../../../../\n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 2-1-18 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n cd ../../../../../\n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+
+
+        f"echo 3-2-1 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-2 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n unset  PWD       \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-3 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n export PWD=xxxxx \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-4 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n export PWD=$HOME \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-5 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n export PWD=/     \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-6 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n export PWD=//    \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-7 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n export PWD=.     \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-8 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n export PWD=..    \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-9 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n export PWD=../.. \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-2-10 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n export PWD=../../../../../ \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-1-11 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n cd /             \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-1-12 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n cd //            \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-1-13 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n cd ///           \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-1-14 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n cd .             \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-1-15 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n cd ..            \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-1-16 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n cd ../../        \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-1-17 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n cd ../../../../../\n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
+        f"echo 3-1-18 current        \n {PATH_ADD}\n{MKDIR} && rm -rf ../test_dir \n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD} \n cd ../../../../../\n {SHOW_PWD} \n __SHELL__\n {SHOW_PWD}",
     ]
 
     test_res |= test("ft_pwd", pwd_test, False, False)
