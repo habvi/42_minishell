fa5dc6a9e59f100e68b1c00d3c6bfb7910aa8f5f
habvi <93956895+habvi@users.noreply.github.com>
2023-07-25 00:47:23 +0900

    add: more test wildcard

--- test/integration_test/run_expansion.py
+++ test/integration_test/run_expansion.py
@@ -1,41 +1,99 @@
 from test_function.test_functions import test
+from itertools import permutations
 
-# for wildcard
-DIR_NAME = "test_wildcard"
-MAKE_TEST_DIR = f"mkdir -p test_wildcard && cd {DIR_NAME} && "
-RANDOM_FILES = "z1 zz2 3z 4zz zz5z z6zz z7z7z z8zz8zz zz9z9zzz"
-TOUCH_TEST_Z = f"{MAKE_TEST_DIR} touch {RANDOM_FILES}"
-SORT_COMPARE = f" | tr ' ' '\\n' | sort && cd ../ && rm -rf {DIR_NAME}"
 
-def main():
-    test_res = 0
-
-    word_splitting_test = [
-
-                    ]
+def wildcard():
+    DIR_NAME = "test_wildcard"
+    MAKE_TEST_DIR = f"mkdir -p test_wildcard && cd {DIR_NAME} && "
+    WORDS = "abb**x"
+    random_files = ""
+    for per in permutations(WORDS):
+        random_files += ''.join(per) + " "
+    random_files = random_files.replace("x", "'*'")
+    TOUCH_TEST_FILES = f"{MAKE_TEST_DIR} touch {random_files} \n "
+    SORT_COMPARE = f" | tr ' ' '\\n' | sort && cd ../ && rm -rf {DIR_NAME}"
 
     wildcard_test = [
-                    f"echo * | {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo * {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo z* {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo *z {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo *z* {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo **z* {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo *z** {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo **z** {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo *nosuch {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo nosuch* {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo *nosuch* {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo **nosuch* {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo *nosuch** {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo **nosuch** {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo * * {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo * z* {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo z* * {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo z* *z {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo * z* *z {SORT_COMPARE}",
-                    f"{TOUCH_TEST_Z} echo * z* *nosuch *z {SORT_COMPARE}",
+                    f"echo * {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo ** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo ******* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * * {SORT_COMPARE}",
+
+                    f"{TOUCH_TEST_FILES} echo a* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *a {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *a* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **a* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *a** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **a** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *nosuch {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo nosuch* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *nosuch* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **nosuch* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *nosuch** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **nosuch** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * a* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo a* * {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo a* *a {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * a* *a {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * a* *nosuch *a {SORT_COMPARE}",
+
+                    f"{TOUCH_TEST_FILES} echo b* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *b {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *b* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **b* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *b** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **b** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * b* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo b* * {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo b* *b {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * b* *b {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * b* *nosuch *b {SORT_COMPARE}",
+
+                    f"{TOUCH_TEST_FILES} echo ab* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo ba* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo ab** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo ba** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo a*b {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo b*a {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo b**a {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *ab {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *ba {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **ab {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **ba {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *ab* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **ab* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *ab** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo **ab** {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *a*b {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *b*a {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo a*b* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo b*a* {SORT_COMPARE}",
+
+                    f"{TOUCH_TEST_FILES} echo * *a b* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *a b* * {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo b* *a* *b {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * b* *b *ab {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo * b* *nosuch a*b* *b {SORT_COMPARE}",
+
+                    f"{TOUCH_TEST_FILES} echo '*'* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *'*' {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *'*'* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *'*'a* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *a'*'* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *'*'b* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *b'*'* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo *b'*'b* {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo '*'*'*' {SORT_COMPARE}",
+                    f"{TOUCH_TEST_FILES} echo '*'*'*'* {SORT_COMPARE}",
                     ]
+
+    return wildcard_test
+
+
+def main():
+
+    wildcard_test = wildcard()
 
     expansion_test = [
                     "$",
@@ -73,10 +131,15 @@ def main():
                     "export TEST=libft && cd \'$TEST\' && pwd",
                     ] # todo more test
 
+    word_splitting_test = [
 
-    # test_res |= test("word_splitting", word_splitting_test, False, False)
+                    ]
+
+    test_res = 0
+
     test_res |= test("wildcard", wildcard_test, False, False)
     test_res |= test("expansion", expansion_test, False, False)
+    # test_res |= test("word_splitting", word_splitting_test, False, False)
 
     return test_res
 
